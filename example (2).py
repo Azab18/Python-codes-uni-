b594f58682f7dc92fd1dcbd8cab9d798c5203cab
Ziad Azab <100707085+Azab18@users.noreply.github.com>
2025-10-06 04:54:06 +0300

    Add files via upload

---
+++ example (2).py
@@ -0,0 +1,34 @@
+#p = float(input("enter principle val"))
+#r = float(input("annual interest  rate"))
+#n = float(input("no of times interest is paid"))
+#t = float(input("no of years"))
+#fv = p*(1+(r/n))**(n*t)
+#print(round(fv, 2))
+
+
+#x1 = float(input("enter x1"))
+#x2 = float(input("enter x2"))
+#y1 = float(input("enter y1"))
+#y2 = float(input("enter y2"))
+
+
+#eq = (((x2-x1)**2)+((y2-y1)**2))**0.5
+#print(eq)
+
+
+#n = int(input("enter number"))
+#print(n+2*n+3*n)
+
+
+#email = str(input("please enter yor email \n"))
+#name = email[0:4]
+#print("your name is:\n",name, "hi")
+
+
+#a = float(input("please enter the side of the hexagon"))
+#area = 0.25*((5(5)+(2*(5)**0.5))**0.5)*a**2
+
+#print("the area is:\n", area)
+
+
+
